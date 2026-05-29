4482b8a94e130c9de5981e7b37285fe4d53e061f
rodrig0o0 <rodrigo_santos-fi@estudante.sesisenai.org.br>
2026-05-29 13:35:27 -0300

    salve fast

--- Assets/Scripts/Guitar/PontuacaoFaseUva.cs
+++ Assets/Scripts/Guitar/PontuacaoFaseUva.cs
@@ -1,17 +1,36 @@
 using UnityEngine;
+using TMPro; // Necessário para usar o TextMeshPro
 
 public class PontuacaoFaseUva : MonoBehaviour
 {
     [SerializeField] private int pontos;
+    [SerializeField] private TextMeshProUGUI textoPontuacao; // Arraste seu texto do Canvas para cá no Inspetor
+
+    void Start()
+    {
+        AtualizarInterface();
+    }
 
     public int AddPontos(int addPontos)
     {
-        return pontos += addPontos;
+        pontos += addPontos;
+        AtualizarInterface();
+        return pontos;
     }
 
     public int RedPontos(int redPontos)
     {
-        return pontos -= redPontos;
+        pontos -= redPontos;
+        AtualizarInterface();
+        return pontos;
     }
-    
+
+    // Método auxiliar para manter o texto sempre atualizado
+    private void AtualizarInterface()
+    {
+        if (textoPontuacao != null)
+        {
+            textoPontuacao.text = "Pontos: " + pontos;
+        }
+    }
 }
